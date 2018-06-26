42b710a1257895c33593df636a97fb7a7e76318d
Andreas Lingenhag <11538311+alingenhag@users.noreply.github.com>
2018-06-26 14:45:36 +0200

    Add api doc.

--- account-service/src/main/java/ch/fuzzle/account/controller/AccountController.java
+++ account-service/src/main/java/ch/fuzzle/account/controller/AccountController.java
@@ -2,8 +2,12 @@ package ch.fuzzle.account.controller;
 
 import ch.fuzzle.account.service.AccountService;
 import ch.fuzzle.model.AccountRequest;
-import java.math.BigDecimal;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
@@ -18,6 +22,12 @@ class AccountController {
         this.service = accountService;
     }
 
+
+    @ApiOperation(value = "find by name", notes = "Find an existing account by firstname and lastname. ")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal Server Error")})
     @ResponseBody
     @GetMapping(value = "/account/{firstname}-{lastname}", produces = APPLICATION_JSON_UTF8_VALUE)
     public AccountRequest findByName(@PathVariable String firstname, @PathVariable String lastname) {
@@ -29,6 +39,11 @@ class AccountController {
         return accountRequest;
     }
 
+    @ApiOperation(value = "get balance", notes = "Return the balance of the requested account. ")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal Server Error")})
     @ResponseBody
     @GetMapping(value = "/balance/{firstname}-{lastname}", produces = APPLICATION_JSON_UTF8_VALUE)
     public BigDecimal getBalance(@PathVariable String firstname, @PathVariable String lastname) {
